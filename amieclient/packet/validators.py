e9fd2f0bcf355fe63135f7757aa324e207ac0163
G. Ryan Sablosky <sablosky@psc.edu>
2021-03-31 10:19:02 -0400

    Fix ResourceList validator to work with reply pkts
    Reply packets may not have to specify a ResourceList attribute, but if
    they do, we should still check if its valid.

--- amieclient/packet/validators.py
+++ amieclient/packet/validators.py
@@ -11,8 +11,11 @@ def _validate_resource_list(pkt):
     ResourceLists must only have one element in them.
     A bit weird, yes, but that's the spec.
     """
-    rlist = pkt._required_data.get('ResourceList', [])
-    if len(rlist) != 1:
-        raise PacketInvalidData("ResourceList must have exactly one element")
+    rlist = pkt._required_data.get('ResourceList')
+    if rlist is not None:
+        if not isinstance(rlist, list):
+            raise PacketInvalidData("ResourceList must be a list")
+        if len(rlist) != 1:
+            raise PacketInvalidData("ResourceList must have exactly one element")
 
     return True
